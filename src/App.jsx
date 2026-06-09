a5928c5c74f68d421b8e01194cec7000f9fb83ad
facu-jc <camanof94@gmail.com>
2026-06-09 02:16:54 -0300

    feat: agregar página 404 personalizada y actualizar rutas

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Complexes from './pages/Complexes';
 import ContactPage from './pages/Contact';
 import OwnerDashboard from './pages/Owner/OwnerDashboard';
 import useAuthStore from './store/authStore';
+import Error404 from "./pages/Error404";
 import './App.css';
 
 function ProtectedRoute({ children, role }) {
@@ -48,7 +49,10 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/" replace />} />
+
+        <Route path="/404" element={<Error404 />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />/
       </Routes>
     </BrowserRouter>
   );
