d2fada1336f62ef290a91694ba0af83f5d3425c1
Aldana Oyola <lizaldanaoyolaruiz@gmail.com>
2026-06-09 12:51:22 -0300

    fix: el uso de pagina de detalle como pagina y no como seccion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import FeaturedClubs from '../components/FeaturedClubs';
-import Solutions from '../components/Solution360'; // <-- Importar esto
+import Solutions from '../components/Solution360';
 import Footer from '../components/Footer';
 import './Home.css';
 
@@ -11,8 +10,7 @@ const Home = () => {
       <Navbar />
       <main>
         <Hero />
-        <FeaturedClubs />
-        <Solutions /> {/* <-- Agregar esto */}
+        <Solutions />
       </main>
       <Footer />
     </div>
