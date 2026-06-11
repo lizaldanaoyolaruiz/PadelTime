db590a125cd6499c9220def81bd8d404df5273b2
Aldana Oyola <lizaldanaoyolaruiz@gmail.com>
2026-06-11 17:24:03 -0300

    feat: agregar componentes de esqueleto para carga de datos en la interfaz

---
+++ src/pages/SuperAdmin/components/Skeletons.jsx
@@ -0,0 +1,27 @@
+export function SkeletonRow() {
+  return (
+    <tr>
+      {[...Array(6)].map((_, i) => (
+        <td key={i}><div className="gc-skeleton" /></td>
+      ))}
+    </tr>
+  );
+}
+
+export function SkeletonStatCard() {
+  return (
+    <div className="gc-stat-card">
+      <div className="gc-skeleton" style={{ width: 44, height: 44, borderRadius: 10, marginBottom: 12 }} />
+      <div className="gc-skeleton" style={{ width: 60, height: 28, marginBottom: 8, borderRadius: 6 }} />
+      <div className="gc-skeleton" style={{ width: 100, height: 14, borderRadius: 4 }} />
+    </div>
+  );
+}
+
+export function SkeletonMobileCard() {
+  return (
+    <div className="gc-mobile-card">
+      <div className="gc-skeleton" style={{ height: 72, borderRadius: 8 }} />
+    </div>
+  );
+}
