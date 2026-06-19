6abeb6cfcc5f9e014655f9b5606123eb5f030e45
Octavio <octaviofernandez40999@gmail.com>
2026-06-19 02:13:22 -0300

    feat: Añadir función getSlotsCalendar para obtener horarios disponibles

--- src/services/reservationService.js
+++ src/services/reservationService.js
@@ -1,7 +1,8 @@
 import api from './axios';
 
-export const getReservasOwner = (params)  => api.get('/bookings', { params });
-export const createReserva    = (data)    => api.post('/bookings', data);
-export const confirmarReserva = (id)      => api.patch(`/bookings/${id}/confirm`);
-export const rechazarReserva  = (id, nota) => api.patch(`/bookings/${id}/reject`, { reason: nota });
-export const cancelarReserva  = (id)      => api.patch(`/bookings/${id}/cancel`);
+export const getReservasOwner  = (params)       => api.get('/bookings', { params });
+export const getSlotsCalendar  = (params)       => api.get('/bookings/slots', { params });
+export const createReserva     = (data)         => api.post('/bookings', data);
+export const confirmarReserva  = (id)           => api.patch(`/bookings/${id}/confirm`);
+export const rechazarReserva   = (id, nota)     => api.patch(`/bookings/${id}/reject`, { reason: nota });
+export const cancelarReserva   = (id)           => api.patch(`/bookings/${id}/cancel`);
