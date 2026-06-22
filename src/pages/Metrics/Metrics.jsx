9fbf352d867c94850c86b7c68ba51e4e308e078b
facu-jc <camanof94@gmail.com>
2026-06-22 14:23:30 -0300

    feat: añadir importación de useEffect y servicio para obtener métricas

--- src/pages/Metrics/Metrics.jsx
+++ src/pages/Metrics/Metrics.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState,  useEffect } from "react";
 import "./Metrics.css";
+import { getMetrics } from "../../services/metricsService";
 
 import {
   ResponsiveContainer,
