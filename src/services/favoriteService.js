4821ed7176996f2af31319811617f46e8163d01a
Octavio <octaviofernandez40999@gmail.com>
2026-06-23 14:40:58 -0300

    fix: agregar función para obtener favoritos desde la API

--- src/services/favoriteService.js
+++ src/services/favoriteService.js
@@ -1,5 +1,6 @@
 import api from './axios';
 
+export const getFavoritos    = ()           => api.get('/favorites');
 export const checkFavorito   = (complexId) => api.get(`/favorites/${complexId}/check`);
 export const agregarFavorito = (complexId) => api.post(`/favorites/${complexId}`);
 export const quitarFavorito  = (complexId) => api.delete(`/favorites/${complexId}`);
